5767e420b674764de0a4914ef0cc5752ae0e05aa
Horiyasiddique <horiyasiddique52@gmail.com>
2024-11-16 20:20:32 +0500

    improvements

--- e-commerce-website2/src/app/layout.tsx
+++ e-commerce-website2/src/app/layout.tsx
@@ -1,12 +1,8 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-
-
 import Header from "@/components/Header";
 import Hero from "@/components/Hero";
-import Footer from "@/components/Footer";
-
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
